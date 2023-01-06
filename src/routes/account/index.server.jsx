7a57c4e4b0fe4bf11cad2840939c92114ff89e24
Natalie Oldroyd <natalie.oldroyd@shopify.com>
2023-01-06 10:57:38 -0330

    try again

--- src/routes/account/index.server.jsx
+++ src/routes/account/index.server.jsx
@@ -31,7 +31,7 @@ export default function Account({response}) {
   } = useLocalization();
   const {customerAccessToken} = useSession();
 
-  if (!customerAccessToken) return response.redirect('/account/myshopify.com');
+  if (!customerAccessToken) return response.redirect('nataliestaffstore.account.myshopify.com');
 
   const {data} = useShopQuery({
     query: CUSTOMER_QUERY,
@@ -45,7 +45,7 @@ export default function Account({response}) {
 
   const {customer, featuredCollections, featuredProducts} = data;
 
-  if (!customer) return response.redirect('/account/myshopify.com');
+  if (!customer) return response.redirect('nataliestaffstore.account.myshopify.com');
 
   // The logged-in analytics state.
   useServerAnalytics({
--- src/routes/account/orders/[id].server.jsx
+++ src/routes/account/orders/[id].server.jsx
@@ -28,7 +28,7 @@ export default function OrderDetails({response}) {
   } = useLocalization();
   const {customerAccessToken} = useSession();
 
-  if (!customerAccessToken) return response.redirect('/account/myshopify.com');
+  if (!customerAccessToken) return response.redirect('nataliestaffstore.account.myshopify.com');
   if (!id) return response.redirect('/account/');
 
   const {data} = useShopQuery({
